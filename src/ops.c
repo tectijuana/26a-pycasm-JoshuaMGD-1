db12bb8d9bcbfcb67303ef0334c271f942fdd8f3
JoshuaMGD <moniiris5120@gmail.com>
2026-04-15 07:57:27 -0700

    Update ops.c

--- src/ops.c
+++ src/ops.c
@@ -1,1 +1,27 @@
+/*
+ * Autor: Joshua Daniel Moreno Gonzalez
+ * Descripcion: Wrappers en C para funciones ARM64 Assembly
+ * Fecha: 2026-04-14
+ */
 
+#include "ops.h"
+
+int c_sum(int a, int b) {
+    return asm_sum(a, b);
+}
+
+int c_sub(int a, int b) {
+    return asm_sub(a, b);
+}
+
+int c_max(int a, int b) {
+    return asm_max(a, b);
+}
+
+int c_array_sum(int *arr, int n) {
+    return asm_array_sum(arr, n);
+}
+
+int c_array_max(int *arr, int n) {
+    return asm_array_max(arr, n);
+}
